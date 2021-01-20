e52a389b7654c448a92e117b24e40928560d5369
oreste-abizera <oresteabizera11@gmail.com>
2021-01-20 11:13:45 +0200

    Adding nodemailer transport

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
 
+var transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "454027c4702609",
+    pass: "0cc2bd91c87607",
+  },
+});
+
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Node js server running on port ${PORT}`));
